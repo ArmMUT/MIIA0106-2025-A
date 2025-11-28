577250ee074164e0aa6febed8299f187953f17d8
Armra <6811130020@mut.ac.th>
2025-11-28 17:10:18 +0700

    อัพเดท code การทดลองที่2

---
+++ Lab02/Variables1/Variables1/Variables1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	int age = 20;
+	float height = 161;
+	string name = "Prodpran Siriyaman";
+	cout << "Student Profile" << endl;
+	cout << "-----------------------" << endl;
+	cout << "Name: " << name << endl;
+	cout << "Age : " << age << endl;
+	cout << "Height: " << height << " cm" << endl;
+	return 0;
+}
---
+++ Lab02/VariablesTest1/VariablesTest1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+	string name = "Prodpran Siriyaman";
+	int age = 20;
+	string studentID = "6811130020";
+	float gpa = 3.78;
+
+	cout << "=========== Student Info ===========" << endl;
+	cout << "Name        : " << name << endl;
+	cout << "Age         : " << age << endl;
+	cout << "Student ID  : " << studentID << endl;
+	cout << "GPA         : " << gpa << endl;
+	cout << "====================================" << endl;
+
+	return 0;
+}
