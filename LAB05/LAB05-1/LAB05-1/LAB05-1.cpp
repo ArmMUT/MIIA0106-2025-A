c5024915caa2c6549c35c25b320c6d006cbcf75f
Armra <6811130020@mut.ac.th>
2025-12-16 17:27:33 +0700

    LAB5

---
+++ LAB05/LAB05-1/LAB05-1/LAB05-1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+double calculateCircleArea(double x)
+{
+    double PI = 3.14159;
+    double area = PI * x * x;
+    return area;
+}
+double rectangleArea(double width, double length)
+{
+    return width * length;
+}
+    int main()
+{
+        //หาพื้นที่วงกลม
+        const double PI = 3.14159;
+        double radius;
+        cout << "Enter the radius of the circle: ";
+        cin >> radius;
+
+        double area1 = calculateCircleArea(radius);
+       // ฟังชันหาพื้นที่สี่่เหลียม    
+    double width, length;
+
+    cout << "Enter width: ";
+    cin >> width;
+
+    cout << "Enter length: ";
+    cin >> length;
+
+    double area2 = rectangleArea(width, length);
+    cout << "CircleArea " << " = " << area1 << endl;
+    cout << "Rectangle area = " << area2 << endl;
+ 
+    return 0;
+   
+}
---
+++ LAB05/LAB05-1/LAB05-3/LAB05-3.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+// Call by Value
+void Value(int a, int b) {
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+void Reference(int& a, int& b) {
+	int temp = a;
+	a = b;
+	b = temp;
+}
+int main() {
+	int a = 10, b = 20;
+	cout << "Before swap:";
+	cout << "a = " << a << ", b = " << b << endl;
+	Value(a, b);   // ส่งค่าแบบcopy (Call by Value)
+	cout << "After swap (Call by Value):";
+	cout << "a = " << a << ", b = " << b << endl;
+
+	Reference(a, b);   // เรียกแบบreference
+	cout << "After swap(Call by Reference):";
+	cout << "a = " << a << ", b = " << b << endl;
+	return 0;
+}
