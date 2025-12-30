9e65a4efc3d24004c469c9ae432f61a0b8fa79ae
Armra <6811130020@mut.ac.th>
2025-12-30 17:10:25 +0700

    LAB07

---
+++ LAB07/LAB0701/LAB0702/LAB0702.cpp
@@ -0,0 +1,16 @@
+﻿// TODO: แสดงค่า score, address และ pointer
+#include <iostream>
+using namespace std;
+int main() {
+	int score = 50;
+	int* ptr = &score;
+	// TODO
+	//แสดงค่า
+	//cout << score << endl;
+	//cout << *ptr << endl;
+	//แสดงadd
+	cout << "address of score\t " << &score << endl;
+	cout << "value of ptr\t\t " << ptr << endl;
+	cout << "address of pointer\t " << &ptr << endl;
+	return 0;
+}
---
+++ LAB07/LAB0701/LAB0706/LAB0706.cpp
@@ -0,0 +1,47 @@
+﻿// TODO: Bubble Sort แบบมากไปน้อย ด้วย pointer
+#include <iostream>
+using namespace std;
+void printArray(int* p, int size) {
+	// TODO
+	cout << "" << endl;
+	for (int i = 0; i < size; i++)
+	{
+		cout << *(p + i) << "\t";
+	}
+}
+void swapValue(int* a, int* b) {
+	// TODO
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+void sortDescending(int* p, int size) {
+	// TODO
+	for ( int i=0;i< size -1; i++)
+	{
+		//
+		for (int j = 0; j < size - 1-i; j++)
+		{
+			if (*(p + j) < *(p + j + 1))
+			{
+				swapValue((p + j), (p + j + 1));
+			}
+		}
+
+	}
+}
+int main() {
+	int arr[] = { 5, 2, 9, 1, 3 };
+	int size = 5;
+	printArray(arr, size);
+	sortDescending(arr, size);
+	printArray(arr, size);
+	return 0;
+}
+
+// 5,2,9,1,2
+//ถ้าน้อยกว่าสลับตำแห่งกัน
+//ตน.ที่1 เทียบ ตน.2
+//5<2 เท็จไม่ต้งอสลับตำแหน่ง 5,2,9,1,2
+
+
